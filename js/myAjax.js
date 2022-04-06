624d97a61af3e2e3020587c02829b129f2e6c000
999666-zk <15539234100zk@gmail.com>
2022-04-06 18:31:48 +0800

    april function MyAjax

---
+++ js/myAjax.js
@@ -0,0 +1,31 @@
+// 这里是封装的ajax
+
+/* 
+参数说明：
+type 请求类型
+url 请求地址
+data 传参
+successfn 成功回调
+errorfn 失败回调
+*/
+
+//封装ajax
+function request(type, url, data, successfn, errorfn) {
+  // 默认选择post 请求
+  type =
+    type == null || type == "" || typeof type == "undefined" ? "get" : type;
+  // 默认不进行传参
+  data = data == null || data == "" || typeof data == "undefined" ? {} : data;
+  $.ajax({
+    type: type,
+    url: url,
+    dataType: "json",
+    data: data,
+    success: function (res) {
+      successfn(res);
+    },
+    error: function (res) {
+      errorfn(res);
+    },
+  });
+}
